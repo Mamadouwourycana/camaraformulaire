21019e0c64980ac64f1388bda8a2315e7074e9b3
Mamadou Woury Camara <mamadouwourycamara88@mail.com>
2026-08-05 01:54:55 +0000

    modification

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <a href="index.php">Accueil</a>
+    <a href="listeauteurs.php">Auteurs</a>
+    <a href="listecat.php">Catégories</a>
+    <a href="listeslivres.php">Livres</a>
+    <a href="ajoutauteurs.php">Ajouter un auteur</a>
+</nav>
+<hr>
